698cb58889d3186d85b8b7f2979f4b4cc609337b
Kyle Chance <kylechance@gmail.com>
2016-10-17 20:22:17 -0400

    added base axios get with then and catch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
+var axios = require('axios');
 
 var port = process.env.PORT || 8081;
 
@@ -11,6 +12,17 @@ server.use(cors());
 
 server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
+});
+
+server.get('/forecast/:lattitude,:longitude', function(request, response){
+  var url = 'http://https://api.darksky.net/forecast/ff86bd244821f095a1f3670ce86ec492/37.8267,-122.4233';
+  axios.get(url)
+        .then(function(forecast){
+          response.send(forecast.data);
+        })
+        .catch(function(error){
+          response.send(error);
+        });
 });
 
 server.listen(port, function(){

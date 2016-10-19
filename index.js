2a275cc768b71d2f77dee8b273fea40df95ed1cb
Kyle Chance <kylechance@gmail.com>
2016-10-19 19:15:08 -0400

    added in auth and fixed h, along with syntax correction

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var server = express();
 var logger = require('./middleware/logger.js');
 var cors = require('cors');
-
+var authorize = require('./middleware/auth.js')
 
 var forecastRouter = require('./routers/forecast.router.js');
-
+var indexRouter = require('./routers/index.router.js');
 var port = process.env.PORT || 8081;
 
 server.use(express.static(__dirname + '/public'));
 server.use(logger);
 server.use(cors());
 
-server.get('/', function(request, response){
-  response.sendFile('public/html/index.html', {root: __dirname});
-});
+server.use(indexRouter);
 server.use(forecastRouter);
 
+
+
 server.listen(port, function(){
-  console.log('Now listening on port');
+  console.log('Now listening on port', port);
 });
